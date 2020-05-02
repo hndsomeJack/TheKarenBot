e761ea14d47e2e6673190733a87ff85094c31694
Garret Donaldson <garretdonaldson@Garrets-MacBook-Air.local>
2020-05-02 15:22:34 -0400

    Initial commit

---
+++ KarenBot.py
@@ -0,0 +1,99 @@
+#! /Users/garretdonaldson/pyProjects/Bots/KarenBot/env/bin/python3
+
+import praw
+import sqlalchemy as db
+import re
+import random as rand
+from sqlalchemy import Column, String, MetaData, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+reddit = praw.Reddit('KarenBot')
+# each subreddit organies by .hot, .new, .controversial, .top, and .gilded
+# can also use engine search via .search("SEARCH_KEYWORDS")
+karenRegex = re.compile(r'a Karen', re.IGNORECASE)
+
+# karenList = ["Keep'm comin', pal, I went through child birth AND child death.", 
+#"Michaelllllllllll!!", "You will call me by my husband's rank"]
+karenList = ["You will call me by my husband's rank", "Keep'm coming, pal, I went through child birth _and_ child death",
+"Um, yeah. I'm gonna have to speak to your manager...", "This is not very _live love laugh_ of you. You should be ashamed of yourself.", 
+"My son's a vegetarian and you've got beef? I expect all downvotes to be removed from this order, and some free gold thrown in.", 
+"I demand this account be deleted.", "I bet you were vaccinated."]
+karenSignature = "_Beep Boop, I'm a b*tch_"
+
+blackList = ['r/suicide', '']
+
+# create database connection and start it  
+Base = declarative_base() # Create initial Base class to defined mapped classes from
+
+engine = db.create_engine('sqlite:///roxy.sqlite3', echo=True) 
+connection = engine.connect()
+Base.metadata.create_all(engine)
+
+# Create session object, which is the ORM's "handle" to the database, allowing conversation withi it
+# use session = Session() whenever need to talk to database
+Session = sessionmaker(bind=engine)
+
+class Comment(Base):
+    __tablename__ = 'comments'
+
+    post_title = Column(String, primary_key=True)
+    comment_id = Column(String, primary_key=True)
+    comment_body = Column(String)
+    user_id = Column(String)
+
+    def __repr__(self):
+        return "<Comments(post_title='%s', comment_id='%s', comment_body='%s', user_id='%s')>" % (
+            self.post_title, self.comment_id, self.comment_body, self.user_id)
+
+
+
+#create dummy user to test
+# dummy_post = Comment(post_title='Testing my SQLite3 database!', comment_id='5h35tf',
+    #comment_body='What the reddit user said!', user_id='dummyRedditUser_69')
+
+#session = Session()
+#session.add(dummy_post)
+#session.commit()
+
+
+
+
+subreddit = reddit.subreddit("TellMeYoureTesting")
+
+# Indefinitely iterate over submissions and comments in All
+for submission in subreddit.stream.submissions():
+
+    postTitle = submission.title # store post title in case a match is found in next loop
+    for comment in subreddit.stream.comments():
+        
+        # store values in case match is found, so can add to Comment table
+        cBody = comment.body
+        cID = comment.id
+        cAuthor = comment.author.name
+
+        karenMatch = karenRegex.search(cBody)
+
+        if karenMatch is None:
+            continue
+        else:
+            print('Match found')
+            session = Session()
+            compareList = []
+            for instance in session.query(Comment.comment_id):
+                compareList.append(instance)
+            if cID not in compareList:
+                comment.reply(karenList[rand.randint(0,3)] + '\n\n' + karenSignature)
+
+            session.add( Comment(post_title=postTitle, comment_id=cID, comment_body=cBody, user_id=cAuthor))
+            print('Replying to comment')
+            
+                
+            session.commit()
+            print(cBody)
+            print(cID)
+            print(cAuthor)
+            print(postTitle)
+
+
